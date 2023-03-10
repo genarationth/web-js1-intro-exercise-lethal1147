8be1f74120227b65e4f2154b3b6da26a4a023bdc
Joakim <yukimever@gmail.com>
2023-03-10 12:01:51 +0700

    add check name condition

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -4,6 +4,8 @@ function ChangeName() {
     let name = prompt('What your name?');
     if (name === '') {
         alert('Please add your name');
+    } else if (!isNaN(name)) {
+        alert('Your name cannot be Number');
     } else {
     inputName.innerHTML = 'Hello ' + name;
     console.log(name)
